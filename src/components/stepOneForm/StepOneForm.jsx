5b2441fa61a67fb178e9468f38f8e096fa17be3a
khamais-maaroufi <khamais.maaroufi@gmail.com>
2022-04-24 23:29:13 +0100

    commit 8

--- src/components/stepOneForm/StepOneForm.jsx
+++ src/components/stepOneForm/StepOneForm.jsx
@@ -9,29 +9,46 @@ export default function StepOneForm() {
 
   useEffect(() => {});
 
-  //the outputs of the activity sector selector
+  //the outputs of the activity sector selector child component
   const [Sa, setSa] = useState("");
   console.log("here is the parent selected sa:", Sa);
 
-  //the outputs of the juridical forme selector
+  //the outputs of the juridical forme selector child component
   const [Fj, setFj] = useState("");
   console.log("here is the parent selected fj:", Fj);
 
+  //props of activity sector
   const propsSA = {
     label: "secteur d'activité",
     options: dropdownOptions,
     name: "activity-sector",
   };
+  const propsSA1 = {
+    label: "secteur d'activité",
+    options: [{ key: "Choisir une secteur d'activité", value: "" },
+    { key: "Profession Libérale", value: "Profession Libérale" }],
+    name: "activity-sector",
+  };
+
+  //props of juridical forme
   const propsFJ = {
     label: "Forme juridique*",
     options: dropdownOptionsFJ,
     name: "Forme-juridique",
   };
+  const propsFJ1 = {
+    label: "Forme juridique*",
+    options: [{ key: "SCI", value: "SCI" }],
+    name: "Forme-juridique",
+  };
+
   const stepOneSubmit = () => {};
   return (
     <div>
       <form onSubmit={stepOneSubmit} className={styles.formControl}>
-        <Select {...propsSA}  changeSA={Sa => setSa(Sa)} />
+        { (Fj === "SELARL" || Fj === "SELEURL" || Fj === "SELAS" || Fj === "SELASU" ) ?
+         <Select {...propsSA1}  changeSA={Sa => setSa(Sa)} /> :
+         <Select {...propsSA}  changeSA={Sa => setSa(Sa)} />}
         <div>
           <label htmlFor="social-reason" className={styles.label}>
             Raison Social
